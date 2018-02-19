84fafe589514e2fdd496f55f5dc9757852404aa4
Thomas Jgenti <tjgenti@gmail.com>
2018-02-19 14:56:37 +0100

    [EDC-1306][EDC-1307] Creation du projet pour le composant et l'app de test

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# edc-popover-react
-The angular edc popover to display the contextual help
+## edc-popover-react
+This is a react implementation of edc popover displaying the contextual help
+
+_This project is meant to be used with **easy doc contents** (aka edc)._
+
+edc is a simple yet powerful tool for agile-like documentation
+management.
+
+Learn more at [https://www.easydoccontents.com](https://www.easydoccontents.com).
+
+## Dependencies
+
+The required dependencies are:
+
+- React JS 16.2.0 or higher
+
---
+++ src/testapp/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">edc Popover React Component</h1>
+        </header>
+        <p className="App-intro">
+          This is an integration example for <a href="https://www.easydoccontents.com">edc</a> Popover in a React app.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
