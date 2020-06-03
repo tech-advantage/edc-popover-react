1149635e261dad3fb170ed8657036700858bb052
Nitorac <nitorac.r@gmail.com>
2020-06-03 10:36:03 +0200

    Written tests and storybook for EdcHelp component

---
+++ src/components/EdcHelp.spec.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import EdcHelp from './EdcHelp'
+
+describe('EdcHelp', () => {
+  it('should render a <i> to display the icon', () => {
+    const wrapper = shallow(<EdcHelp />)
+    expect(wrapper.find('i').length).toEqual(1)
+  })
+})
---
+++ src/components/EdcHelp.stories.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import EdcHelp from './EdcHelp'
+
+export default { title: 'EdcHelp' }
+
+export const withDefaultIcon = () => <EdcHelp />
+
