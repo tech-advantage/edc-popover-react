c8c70cb5e5ff8ca6f3d58ee94b58ec6234978afd
Nitorac <nitorac.r@gmail.com>
2020-06-16 16:05:04 +0200

    Fixed EdcIcon not swallowing events

--- src/components/EdcHelp.tsx
+++ src/components/EdcHelp.tsx
@@ -26,13 +26,18 @@ function EdcIcon(props: EdcIconProps): JSX.Element {
   const cssClass = 'help-icon' + (props.edcHelpProps.dark ? ' on-dark' : '')
 
   if (typeof props.icon === 'string') {
-    return <i className={`${props.icon} ${cssClass}`} />
+    return <i {...props} className={`${props.icon} ${cssClass}`} />
   }
   if (props.icon.type === 'class') {
-    return <i className={`${props.icon.content} ${cssClass}`} />
+    return <i {...props} className={`${props.icon.content} ${cssClass}`} />
   }
   return (
-    <img className={cssClass} src={props.icon.content} alt='Popover icon' />
+    <img
+      {...props}
+      className={cssClass}
+      src={props.icon.content}
+      alt='Popover icon'
+    />
   )
 }
 
